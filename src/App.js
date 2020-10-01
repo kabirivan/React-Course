cc88b1ab9175e0a954c080dbeea5508cdd25a61b
Xavier Aguas <aguas.xavier2013a@gmail.com>
2020-10-01 08:58:39 -0500

    Add component Numbers

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Button from './components/Button'
 import Functions from './components/Functions'
 import MathOperations from './components/MathOperations'
+import Numbers from './components/Numbers'
 import Result from './components/Result'
 import './App.css';
 
@@ -14,18 +14,8 @@ const App = () => {
   return (
     <main className="react-calculator">
       <Result />
-      <div className="numbers">
-        <Button text="1" clickHandler={clickHandlerFunction}/>
-        <button>2</button>
-        <button>3</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button>0</button>
-      </div>
+      
+      <Numbers onClickNumber = {(number)=>{console.log("Number: ", number)}}/>
      
      
      <Functions 
